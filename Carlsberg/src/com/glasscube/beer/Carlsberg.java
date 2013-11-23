01de1bdce0ccdb952ccbd6b3598573612cb33e9f
Anuruddha Premalal <anuruddhapremalal@gmail.com>
2013-11-23 19:20:23 +0530

    stable release 1

--- Carlsberg/src/com/glasscube/beer/Carlsberg.java
+++ Carlsberg/src/com/glasscube/beer/Carlsberg.java
@@ -3,13 +3,14 @@ package com.glasscube.beer;
 import org.apache.cordova.DroidGap;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.os.Handler;
 
 public class Carlsberg extends DroidGap
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+     Handler h = new Handler();
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
     }
